66ce03977f32b4cd38864b17faeb9d4b462e68ab
unknown <mihneacflorescu@gmail.com>
2022-01-19 21:28:23 +0200

    Curs02

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,40 @@
+lista=[8, 2, -5, "string", 2.7]
+# print(type(lista))
+# print(lista[3])
+# print(lista[5]) #daca indexul nu exista, apare o eroare
+# print(lista[1:]) #afisam elementele de la indexul 1 inclusiv pana la final
+# print(lista[1:3]) #afisam elementele de la indexul 1 inclusiv, pana la indexul 3, exclusiv
+# print(lista[1:6]) #afisam elementele de la indexul 1 inclusiv, pana la indexul 6, exclusiv. nu da eroare, desi indexul final e out of rance
+# print(lista[5:]) #lista goala
+# print(lista[5])  #eroare
+# print(lista[0:5:2])  #al treilea argument al slicingului este pasul
+#denumire_lista[index_inceput:index_final-1:pas. In lipsa argumentului pas, pasul este 1
+# print(lista[0]) #afisare primul element
+# print(lista[-1]) #afisare ultimul element
+# print(lista[-1:]) #afisare ultimul element
+# print(lista[-1:-3:-1]) #mers de la final la elementul 3 de la final, cu pas negativ
+# lista.append("mere")
+# lista[3]="mere"
+# lista[5]="mere"
+# print(lista)
+# print(len(lista))
+
+#print(lista.index(2)) #afisarea indexului unui element
+
+# lista.insert(3, "element nou")
+# lista.remove(-5) #se va numi elementul care trebuie scos
+# lista.pop() #se va scoate ultimul element
+# lista.pop(2) #se va scoate de pe indexul 2
+
+# lista.clear() #se sterg toate elementele din lista
+
+# lista.reverse() #se va inversa ordinea elementelor
+
+# lista.pop(3)
+lista_stringuri = ['8','21', '5']
+
+print(ord('8'))
+# lista.sort()
+lista_stringuri.sort()
+
+print(lista_stringuri)
---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,69 @@
+# print ("Numele meu este Codrin")
+
+# print(1, type(1)) #tipul int (intreg)
+# print (1.1, type(1.1)) #type float (rational/zecimal
+# print(1j, type(1j)) #tipul complex
+#print(int(1.1))
+#print(float(1))
+# print(complex(1))
+
+# print(1+1)
+# print(1-1)
+# print(1*0)
+# print(1*3) #inmultire
+# print(5/2) #impartire exact
+# print (5//2) #obtinere cat
+# print(5%2) #obtinere rest (modulo)
+# print(5**2) #ridicare la putere
+
+#Operatii realizate cu rezultat Boolean
+
+# print(5 == 2) #operator de egalitate
+# print(5 != 2) #operator de inegalitate
+# print(5 > 2) #operator de comparare
+# print(5 < 2) #operator de comparare
+# print(5 >= 2) #operator de comparare
+# print(5 <= 2) #operator de comparare
+
+# print(5>2 and 3>1) #operator logic
+# print(5>2 or 3>1) #operator logic
+
+# print(not(5>2 and 3>1)) #operator de negare
+
+# print(7 is 7)
+# print(7 is not 7)
+
+# my_var = 0
+# my_var = None #nicio valoare assignata
+
+# my_var, my_var_1 = 0,0
+# my_var = 3
+# my_var = my_var + 1 #asignare prin incrementare
+# my_var *= 2 #asignare prin inmultire
+# print(my_var)
+
+# print("Ana are mere")
+# print('Ana are mere')
+
+# variabila = "Ana 'are' mere"
+# variabila = 'Ana "are" mere'
+# variabila = "Ana \'are\' \n mere"
+# variabila="""Ana are
+# mere
+# print(variabila)
+
+nume="Florescu"
+prenume="Codrin"
+varsta = 20
+inaltime = 1.65
+# prezentare = "Numele meu este {} si prenumele este {}".format(nume,prenume)
+# prezentare = "Numele meu este {1} si prenumele este {1}".format(nume,prenume)
+# prezentare = "Numele meu este " + nume + " si prenumele meu este " + prenume +" si varsta mea este " + str(varsta)
+# prezentare = "Numele meu este {0} si prenumele meu este {1} si varsta este {2}".format(nume,prenume,varsta)
+# prezentare = f"Numele meu este {nume} si prenumele meu este {prenume} si varsta este {varsta}"
+#prezentare = f"Numele meu este {nume} si prenumele meu este {prenume}."
+# print(prezentare)
+
+# calcul = inaltime + varsta
+calcul = nume + prenume
+print(calcul)
