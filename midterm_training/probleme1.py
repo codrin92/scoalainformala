9a686af980add4c1bf6a4d9c1f0ff5ed5eff97be
unknown <mihneacflorescu@gmail.com>
2022-03-09 20:01:06 +0200

    preg examen

--- midterm_training/probleme1.py
+++ midterm_training/probleme1.py
@@ -19,22 +19,37 @@ def text_replacer(sentence, config_list, variator):
     new_variator = variator + len(target_text) - len(config_list[2])
     return sentence_2, new_variator
 
+def text_replacer_2(sentence, config_list):
+    """Functie care ia stringul si lista care contine intervalele de slice si textul inclouitor, si face inlocuirile
+    Functia foloseste si un parametru variator care aranjeaza marginile intervalelor"""
+
+    target_text = string[config_list[0] - 1:config_list[1]:1]
+    sentence_2 = sentence.replace(target_text, config_list[2])
+    return sentence_2
+
+def patches_loop(propozitie, list):
+    for item in list:
+        result = text_replacer_2(propozitie, item)
+        propozitie = result
+    return propozitie
+
+
 string = "The Inquisitor must meet Varric on top of Skyhold's battlements to be introduced."
 
 patches = [[5, 14, "Conquistador"], [26, 31, "King"], [43, 49, "Palace"]]
 
-var = 0  #Definim o valoare initiala pentru variator.
+patches_2 = patches[::-1]
+
+ #Definim o valoare initiala pentru variator.
 # Acesta este un numar intreg care ajuta la ajustarea marginilor intervalului de slice
 # pe masura ce se inlocuiesc cuvintele din cod. Asta ajuta cu diferentele de lungime intre textul de inlocuit si textul
 # inlocuitor.
 
 print(string)
 
-for item in patches:
-    result = text_replacer(string, item, var)
-    string = result[0]
-    var = result[1]
+string_2=patches_loop(string, patches_2)
 
-print(string)
+print(string_2)
+
 
 
